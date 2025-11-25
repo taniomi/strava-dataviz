8141001ddc5cbe342cb1136a907d678e8a1289be
taniomi <131834608+taniomi@users.noreply.github.com>
2025-11-25 10:15:47 -0300

    feat: build pipeline to send athlete data to Databricks locally
    - Databricks Free can't access APIs
    - Run the pipeline locally, send data to Databricks catalog
    
    close #2

--- pipelines/rest_api_pipeline.py
+++ pipelines/rest_api_pipeline.py
@@ -1,43 +1,18 @@
-# Databricks notebook source
+# Databricks notebook source  # noqa: INP001
+"""This is a REST API pipeline for extracting data from Strava API."""
 # MAGIC %md
 # MAGIC # Setup
 
 # COMMAND ----------
 
-# DBTITLE 1,Install dlt
-# MAGIC %pip install dlt[databricks]>=1.18.2
-
-# COMMAND ----------
-
-# DBTITLE 1,Restart kernel
-# MAGIC %restart_python
-
-# COMMAND ----------
-
-# DBTITLE 1,Import dlt
-import sys
-
-# dlt patching hook is the first one on the list
-metas = list(sys.meta_path)
-sys.meta_path = metas[1:]
-
-import dlt
-sys.meta_path = metas  # restore post import hooks
-
-# COMMAND ----------
-
 # DBTITLE 1,Imports
 import logging
-from typing import Any, Optional
+from typing import Any
 
 import dlt
-from dlt.common.pendulum import pendulum
-from dlt.destinations import databricks
 from dlt.sources.rest_api import (
     RESTAPIConfig,
-    check_connection,
     rest_api_resources,
-    rest_api_source,
 )
 
 logging.basicConfig(level=logging.INFO)
@@ -53,16 +28,18 @@ logging.getLogger("py4j").setLevel(logging.ERROR)
 
 # COMMAND ----------
 
-# DBTITLE 1,Define Strava API source in dlt
+# DBTITLE 1,Define pipeline
+
+# Define Strava API source in dlt
 @dlt.source(name="strava")
-def strava_api_source(api_key: str = dlt.secrets.value) -> Any:
+def strava_api_source(api_token: str = dlt.secrets.value) -> Any:  # noqa: ANN401
     """Create a REST API configuration for the Strava API."""
     config: RESTAPIConfig = {
         "client": {
             "base_url": "https://www.strava.com/api/v3/",
             "auth": {
                 "type": "bearer",
-                "token": api_key,
+                "token": api_token,
             },
         },
         "resource_defaults": {
@@ -80,19 +57,18 @@ def strava_api_source(api_key: str = dlt.secrets.value) -> Any:
                 },
                 "processing_steps": [ # filter and transform data
                 ],
-            }
+            },
         ],
     }
     yield from rest_api_resources(config) # understand what is this `yield from`
 
-# COMMAND ----------
-
-# DBTITLE 1,Define Strava pipeline
+# Define Strava pipeline
 def load_strava() -> None:
+    """Load Strava data into Databricks."""
     pipeline = dlt.pipeline(
         pipeline_name="rest_api_strava",
         destination="databricks",
-        dataset_name="rest_api_data",
+        dataset_name="strava_dataviz",
     )
 
     load_info = pipeline.run(strava_api_source())
@@ -107,3 +83,5 @@ def load_strava() -> None:
 
 # DBTITLE 1,Run pipeline
 load_strava()
+
+# COMMAND ----------
